2a732ee284d60a1ca0cb4a7171595366d1fea927
rodrigo <rodrigo.rgtic@gmail.com>
2020-06-26 09:52:45 -0300

    design: server name

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -3,13 +3,15 @@ import React from 'react';
 import { Grid } from './styles';
 
 import ServerList from '../ServerList';
+import ServerName from '../ServerName';
 
 const Layout: React.FC = () => {
   return (
   <Grid>
     <ServerList />
+    <ServerName />
   </Grid>
   )
-}
+} 
 
 export default Layout;
---
+++ src/components/ServerName/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Container, Title, ExpandIcon } from './styles';
+
+const ServerName: React.FC = () => {
+  return (
+  <Container>
+    <Title>Servidor Rodrigo</Title>
+
+    <ExpandIcon />
+  </Container>
+  )
+}
+
+export default ServerName;
